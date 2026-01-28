f1e85542c3ffb3a093cc56d1858ab906938b4e60
ihrarkhan113-cyber <ihrarkhan113@gmail.com>
2026-01-28 12:37:40 +0500

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/repository-name/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          firebase: ['firebase/app', 'firebase/firestore']
+        }
+      }
+    }
+  }
+});
